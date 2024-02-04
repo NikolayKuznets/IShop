9c2fb1814bd7bb2f2961db851fed14bad4103ce4
NikolayKuz <nikolaykuz@yahoo.com>
2024-02-04 16:39:34 +0300

    Initial Commit

---
+++ IShop/IShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  IShopApp.swift
+//  IShop
+//
+//  Created by Ник К on 04.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct IShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
